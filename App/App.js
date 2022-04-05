6972a1f249d6152631c12665033a0984987106ec
gigi-luna <98123241+gigi-luna@users.noreply.github.com>
2022-04-05 16:56:30 -0400

    updated app

--- App/App.js
+++ App/App.js
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Welcomescreen from './app/screens/Welcomescreen';
+import React from 'react';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Welcomescreen/>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
