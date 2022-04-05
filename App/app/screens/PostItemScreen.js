10baebaf4e91f97f7b5a3ef7a75a9d5da9697ab1
Joseph <Jcarbon3@stevens.edu>
2022-04-04 20:53:45 -0400

    Added react navigation dependancies

---
+++ App/app/screens/PostItemScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ImageBackground } from 'react-native';
+
+function PostItemScreen(props) {
+    return (
+        <ImageBackground>   </ImageBackground>
+    );
+}
+
+export default PostItemScreen;
