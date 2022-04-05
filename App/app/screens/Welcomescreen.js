2e385fd0986dd0256ff35dcb6a8b02a674b4a4fa
gigi-luna <98123241+gigi-luna@users.noreply.github.com>
2022-04-05 17:01:11 -0400

    added welcome screen

---
+++ App/app/screens/Welcomescreen.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Image, ImageBackground, StyleSheet, View } from 'react-native';
+
+function Welcomescreen(props) {
+    return (
+       <ImageBackground
+       style={styles.background}
+       source={require("../assets/background.png")}>
+           <Image style={styles.logo} source={require('../assets/logo.png')}></Image>
+           
+           <View style={styles.loginButton}></View>
+           <View style={styles.regButton}></View>
+
+       </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+    background:{
+        flex: 1,
+        justifyContent:"flex-end",
+        alignItems: "center",
+    },
+    loginButton:{
+        width: '100%',
+        height: 70,
+        backgroundColor: "indianred",
+    },
+    regButton:{
+        width: '100%',
+        height: 70,
+        backgroundColor: "darkred",
+    },
+    logo:{
+        width: 400,
+        height: 400,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+    }
+
+})
+
+export default Welcomescreen;
